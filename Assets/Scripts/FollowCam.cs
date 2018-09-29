9a97a54161e571ee336f850fd864e2c58fb3d112
Edson Lin <43080740+edsonxx@users.noreply.github.com>
2018-09-28 22:39:18 -0400

    Slingshot will reset

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -18,9 +18,30 @@ public class FollowCam : MonoBehaviour
     }
     void FixedUpdate()
     {
-        // if there's only one line following an if, it doesn't need braces
-        if (POI == null) return; 
-        Vector3 destination = POI.transform.position;
+        Vector3 destination;
+      
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the poi
+            destination = POI.transform.position;
+            // If poi is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile")
+            {
+                // if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view
+                    POI = null;
+                    // in the next update
+                    return;
+                }
+            }
+        }
+
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
